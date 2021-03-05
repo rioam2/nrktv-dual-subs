42cd79ab4cb10fb31ceb61cbc5497495551f77d0
Rio Martinez <rio@riomartinez.com>
2021-03-05 13:41:29 -0500

    add learning mode

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 let globalTL = 'en';
+let globalShowTranslation = true;
 
 // Called for each new caption node mounted to the DOM
 function handleCaptionMount(node) {
@@ -7,6 +8,7 @@ function handleCaptionMount(node) {
 	const req = { action: 'translate', payload: { text, source_lang: 'no', target_lang: globalTL } };
 	chrome.extension.sendRequest(req, ({ error, response }) => {
 		if (!error) {
+			node.setAttribute('hide-translation', !globalShowTranslation);
 			node.setAttribute('data-translation', response);
 		} else {
 			console.error(error);
@@ -45,9 +47,60 @@ const CaptionObserver = (function (handler) {
 	};
 })(handleCaptionMount);
 
-function handleSettingsUpdate({ en, tl }) {
-	en ? CaptionObserver.enable() : CaptionObserver.disable();
-	globalTL = tl;
+const LearningMode = (function () {
+	function setVideoPlayState(play = true) {
+		Array.from(document.querySelectorAll('video.ludo-player')).forEach((player) => {
+			play ? player.play() : player.pause();
+			player.focus();
+		});
+	}
+
+	function setTranslationVisibility(show = true) {
+		const lines = Array.from(document.querySelectorAll('.ludo-captions__line'));
+		lines.forEach((line) => {
+			line.setAttribute('hide-translation', !show);
+		});
+		globalShowTranslation = show;
+	}
+
+	function keyHandler(e) {
+		if (e.key === 't' && !e.repeat) {
+			if (e.type === 'keydown') {
+				console.log('keydown');
+				setTranslationVisibility(true);
+				setVideoPlayState(false);
+			} else {
+				console.log('keyup');
+				setTranslationVisibility(false);
+				setVideoPlayState(true);
+			}
+		}
+		e.target.focus();
+	}
+
+	return {
+		enable() {
+			document.addEventListener('keydown', keyHandler);
+			document.addEventListener('keyup', keyHandler);
+			setTranslationVisibility(false);
+		},
+		disable() {
+			document.removeEventListener('keydown', keyHandler);
+			document.removeEventListener('keyup', keyHandler);
+			setTranslationVisibility(true);
+		},
+	};
+})();
+
+function handleSettingsUpdate({ en, tl, learnMode }) {
+	if (en) {
+		CaptionObserver.enable();
+		learnMode ? LearningMode.enable() : LearningMode.disable();
+		globalTL = tl;
+	} else {
+		CaptionObserver.disable();
+		LearningMode.disable();
+	}
 }
 
 function handleSettingsChanges(changes) {
@@ -60,6 +113,7 @@ function handleSettingsChanges(changes) {
 		handleSettingsUpdate({
 			en: settings['settings-en'],
 			tl: settings['settings-tl'],
+			learnMode: settings['settings-learn-mode'],
 		});
 	});
 }
